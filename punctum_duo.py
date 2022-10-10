39b274d3aad176636d8df76f656a202db8e74ea0
Jose Rios <joserios055@gmail.com>
2022-10-09 20:05:58 -0500

    Ego sum project upload, laboravi boni.

--- punctum_duo.py
+++ punctum_duo.py
@@ -1,61 +1,63 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct  7 22:49:17 2022
+Created on Fri Oct  7 ‏‎10:27:59 2022
 
 @author: jario
 """
 
-import random
+import os
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+#Librería de scikit_learn para regresion lineal
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import RANSACRegressor
+from random import seed
 
-        
-def Random():
-    numerus = random.randint(1, 300)
-    print('Numerus ' + str(numerus))
-    
-def main():
-    
-    experimenta_matrix = []
-    experimenta_nomen = []
+np.random.seed(158)
+#Generamos datos aleatorios
+x1 = np.random.randint(50, 100, 85)
+#Le ingresamos ruido
+ruido = np.random.normal(0, 20, 85)
+y1 = 2 + 3 * x1 + ruido
+#x = np.append(x1,1)
+#y = np.append(y1,1)
 
-    experimenta = int(input("Intra numerum experimentorum: "))
-    for x in range(0,experimenta):
-        
-        globals()['experimentum%s' % x]  = 0
-        globals()['matrix%s' % x] = []
-        #i = len(matrix)
-        folium = 0
-        while folium <= 299:
-            
-            alef = Random()
-            
-            if alef not in globals()['matrix%s' % x]:
-                globals()['matrix%s' % x].append(alef)
-                folium = folium + 1
-                globals()['experimentum%s' % x] = globals()['experimentum%s' % x] + 1
-                print("Folium est ingressus")
-            
-            elif alef in globals()['matrix%s' % x]:
-                folium = folium
-                globals()['experimentum%s' % x] = globals()['experimentum%s' % x] + 1
-                print("Non intravit, iam exstat")
-        
-        experimenta_matrix.append(globals()['experimentum%s' % x])
-        experimenta_nomen.append('fact'+str(x))
-        print("Experimentorum numero: " + str(x) + "Totalis numerus foliorum: " + str(globals()['experimentum%s' % x]) ) 
-        print(globals()['matrix%s' % x])
-        #print(experimenta_nomen)
-        
-        x_valorem = experimenta_nomen
-        y_valorem = experimenta_matrix
-        
-        plt.bar(x_valorem, y_valorem) #El gráfico 
-        plt.title('Experimenta - Totalis numerus foliorum implere album ') #El título 
-        ax = plt.subplot() #Axis 
-        ax.set_xticks(x_valorem) #Eje x 
-        ax.set_xticklabels(x_valorem) #Etiquetas del eje x 
-        ax.set_ylabel('Totalis numerus foliorum') #Nombre del eje y
-        ax.set_xlabel('Experiementa numerus') #Nombre del eje x
-       
-main()
+lw = 2
+plt.scatter(x1, y1, color='yellowgreen', marker='.',
+            label='Tendencia principal')
+plt.legend(loc='upper left', fontsize='x-small')
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
 
+# Se requiere que el arreglo este en forma de columna
+x1 = x.reshape(-1,1)
+y1 = y.reshape(-1,1)
+lr = LinearRegression()
+lr.fit(x1, y1)
+# Muestre el intercepto y el coeficiente
+lr.intercept_, lr.coef_
+ransac = RANSACRegressor(LinearRegression())
+ransac.fit(x1, y1)
+# El objeto guarda los inliers
+inlier_mask = ransac.inlier_mask_
+outlier_mask = np.logical_not(inlier_mask)
+# Variables que se pueden usar luego si se desea
+outliers_x = x1[outlier_mask]
+outliers_y = y1[outlier_mask]
+line_x = np.arange(x1.min(), x1.max())[:, np.newaxis]
+line_y = lr.predict(line_x)
+line_y_ransac = ransac.predict(line_x)
+# Muestre el intercepto y el coeficiente
+ransac.estimator_.intercept_, ransac.estimator_.coef_
+lw = 2
+plt.scatter(x1[inlier_mask], y1[inlier_mask], color='yellowgreen', marker='.',
+            label='Tendencia principal')
+plt.plot(line_x, line_y, color='navy', linewidth=lw, label='Regresor lineal')
+plt.plot(line_x, line_y_ransac, color='cornflowerblue', linewidth=lw,
+         label='Regresor Ransac')
+plt.legend(loc='upper left', fontsize='xx-small')
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
--- punctum_quattour.py
+++ punctum_quattour.py
@@ -1,26 +1,141 @@
-from zipfile import ZipFile
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  9 ‏‎11:48:25 2022
+
+@author: jario
+"""
+
 import pandas as pd
-import requests
+import zipfile
+import wget
 
-url_stream = 'https://www.bl.uk/bibliographic/downloads/NationalParksResearcherFormat_202112_csv.zip'
-my_stream_file = requests.get(url_stream, stream=True)
-path = 'datos.zip'
+def donwload():
+    url = 'https://www.bl.uk/bibliographic/downloads/NationalParksResearcherFormat_202112_csv.zip'
+    wget.download(url, 'C:/Users/jario/Documents/UDEM/Data_analisys/Tasks/Opus_iose/data/NationalParks.zip')
 
-with open(path, 'wb') as f:
-    for ch in my_stream_file:
-        f.write(ch)
+def decompress():
+    zipfilename = "data/NationalParks.zip"
+    password = None
 
-with ZipFile('datos.zip', 'r') as zip:
-    zip.extractall('datos')
+    #aperire et eliciunt omnia files in ZIP
+    z = zipfile.ZipFile(zipfilename, "r")
+    try:
+        z.extractall('data/National_Parks')
+        print('\n File is unzipped in data/NationalParks folder')
+    except:
+        print('Error')
+        pass
+    z.close()
     
-datos = pd.read_csv('datos/topics.csv')
-datos.head(10)
+def main():
+    
+    #donwload()
+    #decompress()
+    
+    # ------  aperi file ---------
+    data_topics = pd.read_csv('data/National_Parks/topics.csv')
 
-Topics = pd.pivot_table(datos,columns=['Topic'], aggfunc='size')
-ordenado = dups_topics.sort_values(ascending = False).sort_values(ascending = False)
+    #Extraemos el pais para llenar los NAN - muchos nombres contienen su ciudad de publicación 
+    country = data_topics['Country of publication'].unique()
+    country_new = []
 
-Tabla = pd.DataFrame()
-Tabla['Nombre'] = ordenado.index
-Tabla['Repeticiones'] = ordenado.values
-Tabla.head()
+    for i in range(len(data_topics)):
+        
+        if 'Wales' in data_topics['Topic']:
+            country_new.append('Wales')
+    
+        elif 'United Kingdom Miscellaneous Islands' in data_topics['Topic']:
+            country_new.append('United Kingdom Miscellaneous Islands')
+    
+        elif 'England' in data_topics['Topic']:
+            country_new.append('England')
+        
+        elif 'Scotland' in data_topics['Topic']:
+            country_new.append('Scotland')
+        
+        elif 'United Kingdom' in data_topics['Topic']:
+            country_new.append('United Kingdom')
+        
+        elif 'Switzerland' in data_topics['Topic']:
+            country_new.append('Switzerland')
+    
+        elif 'Singapore' in data_topics['Topic']:
+            country_new.append('Singapore')
+    
+        elif 'Brazil' in data_topics['Topic']:
+            country_new.append('Brazil')
+        
+        elif 'Germany' in data_topics['Topic']:
+            country_new.append('Germany')
 
+        elif 'France' in data_topics['Topic']:
+            country_new.append('France')
+
+    #j = (len(data_topics))
+    # ------  topics -------------
+    dups_topics = pd.pivot_table(data_topics,columns=['Topic','Country of publication'], aggfunc='size').sort_values(ascending = False)
+    dups_topics = pd.pivot_table(data_topics,columns=['Topic'], aggfunc='size').sort_values(ascending = False)
+    columns_name = dups_topics.index[0:100]
+    columns_points = []
+    columns_country = []
+    #column_name = columns_name[1]
+    index_logos = []
+
+    for i in range(100):
+        columns_points.append(dups_topics[i])
+        columns_country.append(data_topics.loc[i,"Country of publication"])
+        index_logos.append(i)
+
+    #print(columns_points)
+    #name = columns_name[0]
+    #topics['Country of publication'] = 
+    #filter = data_topics[data_topics["Topic"] == name]
+    #data_topics.loc[10,"Country of publication"]
+    topics = pd.DataFrame()
+    topics['Name'] = columns_name
+    topics['Points'] = columns_points
+    topics['Country'] = columns_country
+    #topics.head(100)
+
+    countrys = topics['Country'].unique()
+    verbum = len(country)
+
+    final = pd.DataFrame(index= index_logos)
+    for i in range(len(countrys)):
+        name = countrys[i]
+        final[name] = countrys[i]
+
+    filter_wales = topics[(topics["Country"] == "Wales")]
+    filter_uk = topics[(topics["Country"] == "United Kingdom Miscellaneous Islands")]
+    filter_en = topics[(topics["Country"] == "England")]
+    filter_scot = topics[(topics["Country"] == "Scotland")]
+    filter_nan = topics[(topics["Country"].isna())]
+
+    final['Wales'] = filter_wales.Name
+    final['United Kingdom Miscellaneous Islands'] = filter_uk.Name
+    final['England'] = filter_en.Name
+    final['Scotland'] = filter_scot.Name
+    final['NaN'] = filter_nan.Name
+
+    #final['Name'] = columns_name
+    #final.head()
+    #final.head(100)
+
+    print("----------------------------------------------------------------------------------")
+    print("las temáticas (topics) más populares de Wales: ")
+    print(filter_wales.Name)
+    print("----------------------------------------------------------------------------------")
+    print("\n Las temáticas (topics) más populares de United Kingdom Miscellaneous Islands: ")
+    print(filter_uk.Name)
+    print("----------------------------------------------------------------------------------")
+    print("\n las temáticas (topics) más populares de England: ")
+    print(filter_en.Name)
+    print("----------------------------------------------------------------------------------")
+    print("\n las temáticas (topics) más populares de Scotland: ")
+    print(filter_scot.Name)
+    print("----------------------------------------------------------------------------------")
+    print("\n las temáticas (topics) más populares de paises sin nombre: ")
+    print(filter_nan.Name)
+    print("----------------------------------------------------------------------------------")
+
+main()
